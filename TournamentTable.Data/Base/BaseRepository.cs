4fdc975baed7e9bfe6853fdb71900e3ed0bdb5b6
IlyaKazlou <Ilya_Kazlou1@epam.com>
2016-05-23 00:48:53 +0300

    Initial Commit

---
+++ TournamentTable.Data/Base/BaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentTable.Data.Base
+{
+    public class BaseRepository : IRepository
+    {
+        protected readonly string _tblName;
+
+        public BaseRepository(string tblName) 
+        {
+            _tblName = tblName;
+        }
+
+        protected SqlConnection Connection
+        {
+            get { return new SqlConnection(ConfigurationManager.ConnectionStrings["TournamentTableDb"].ConnectionString); }
+        }
+    }
+}
